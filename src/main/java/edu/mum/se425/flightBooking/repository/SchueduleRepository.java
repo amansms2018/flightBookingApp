1b8733f072a5987f84516115912c780515707f97
amanuel <azewgee@gmail.com>
2019-06-13 08:13:03 -0500

    a

---
+++ src/main/java/edu/mum/se425/flightBooking/repository/SchueduleRepository.java
@@ -0,0 +1,9 @@
+package edu.mum.se425.flightBooking.repository;
+
+import edu.mum.se425.flightBooking.model.Schuedule;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SchueduleRepository extends JpaRepository<Schuedule,Long> {
+}
