29f213f980bfd57399cb683d5636991c31e87df0
Ekubay Brhane <ek@Ekubays-MacBook-Pro.local>
2019-06-19 03:43:31 -0500

    Checkin class added by Ekubay

--- src/main/java/edu/mum/se425/flightBooking/controller/ContactInfoController.java
+++ src/main/java/edu/mum/se425/flightBooking/controller/ContactInfoController.java
@@ -6,9 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import edu.mum.se425.flightBooking.model.ContactInfo;
 import edu.mum.se425.flightBooking.serviceImpl.ContactInfoServiceImpl;
+
 
 
 
@@ -18,12 +21,29 @@ public class ContactInfoController {
 	@Autowired
 	private ContactInfoServiceImpl impl;
 	
-	
+	//showing list of messages to the admin
 	@GetMapping(value="/list")
 	public String showAllMessage(Model model) {
 		List<ContactInfo> allMessages = impl.getListCustomerMessages();
 		
 		model.addAttribute("contacts", allMessages);
 		return "home/contactList";
+	}
+	
+	//displaying contact form for the user
+	@GetMapping(value ="contactus")
+	public String showContactForm(Model model) {
+		ContactInfo contactInfo = new ContactInfo();
+		model.addAttribute("info", contactInfo);
+		
+		return "customer/contactInfoForm";
+	}
+	
+	
+	//Saving customer messages to a database
+	@PostMapping(value = "/save")
+	public String sendMessage(@ModelAttribute("cust") ContactInfo message) {
+		impl.sendMessage(message);
+		return "redirect:/thanks"; //need to create thanks form
 	}
 }
---
+++ src/main/java/edu/mum/se425/flightBooking/model/Checkin.java
@@ -0,0 +1,92 @@
+package edu.mum.se425.flightBooking.model;
+
+import java.time.LocalTime;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name ="check_in")
+public class Checkin {
+
+		private Long id;
+		private String lastName;
+		private String email;
+		private LocalTime chechinTime;
+		private String chechinMethod;
+		
+		//constructor
+		public Checkin() {
+			
+		}
+
+		
+		
+		public Checkin(String lastName, String email, LocalTime chechinTime, String chechinMethod) {
+			super();
+			this.lastName = lastName;
+			this.email = email;
+			this.chechinTime = chechinTime;
+			this.chechinMethod = chechinMethod;
+		}
+
+
+
+		//getters and setters
+		public Long getId() {
+			return id;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+
+		public LocalTime getChechinTime() {
+			return chechinTime;
+		}
+
+		public void setChechinTime(LocalTime chechinTime) {
+			this.chechinTime = chechinTime;
+		}
+
+		public String getChechinMethod() {
+			return chechinMethod;
+		}
+
+		public void setChechinMethod(String chechinMethod) {
+			this.chechinMethod = chechinMethod;
+		}
+
+		public String getLastName() {
+			return lastName;
+		}
+
+
+
+		public void setLastName(String lastName) {
+			this.lastName = lastName;
+		}
+
+
+
+		public String getEmail() {
+			return email;
+		}
+
+
+
+		public void setEmail(String email) {
+			this.email = email;
+		}
+
+
+
+		//to string method
+		@Override
+		public String toString() {
+			return "Checkin [id=" + id + ", chechinTime=" + chechinTime + ", chechinMethod=" + chechinMethod + "]";
+		}
+		
+		
+		
+}
